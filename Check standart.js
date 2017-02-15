50f30acd17c9bed26b8032b89f5cf41a8ac8c24c
Vladimir Mazhara <Vladimir_Mazhara@epam.com>
2017-02-15 03:33:36 +0300

    update <if, ? operators>

---
+++ Check standart.js
@@ -0,0 +1,6 @@
+var check_standart;
+check_standart=prompt('Каково "официальное" название JavaScript?', '');
+if (check_standart=='ECMAScript') 
+	alert('Верно!');
+else
+	alert('Не знаете? "ECMAScript"!');
